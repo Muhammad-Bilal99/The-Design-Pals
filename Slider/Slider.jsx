bf905a4a3ea5e94e0446f627b3427266fb08d73b
Muhammad Bilal <beastop975@gmail.com>
2024-08-07 21:01:13 +0500

    Added Slider

---
+++ Slider/Slider.jsx
@@ -0,0 +1,34 @@
+import Carousel from 'react-bootstrap/Carousel';
+import EmbroideryImage from '/Users/DELL 5480/Desktop/The Design Pals/The-Design-Pals/src/assets/embroidery.jpg';
+
+const Slider = () => {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <EmbroideryImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <EmbroideryImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <EmbroideryImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slider;
