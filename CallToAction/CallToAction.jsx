11f6c1539b64102675d4b9acd762737b9b6ae597
Muhammad Bilal <beastop975@gmail.com>
2024-08-16 11:45:47 +0500

    Made CTA

---
+++ CallToAction/CallToAction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CallToAction.css';
+
+const CallToAction = () => {
+  return (
+    <div className="call-to-action">
+      <h2 className="cta-heading">Transform Your Vision into Reality <br/> Partner up with the Best!</h2>
+      <a href="/request-quote" className="cta-button">Request Your Free Quote</a>
+    </div>
+  );
+};
+
+export default CallToAction;
